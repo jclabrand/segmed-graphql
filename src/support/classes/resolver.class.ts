49af66d4ecb483a8e38540b6840950ac3f803d80
Juan Carlos <jcharlylc@gmail.com>
2024-03-22 12:50:05 -0400

    Publisher on resolver

--- src/support/classes/resolver.class.ts
+++ src/support/classes/resolver.class.ts
@@ -1,4 +1,6 @@
 
+import { PubSub } from 'graphql-subscriptions'
+
 import { IEvent } from '../types'
 
 
@@ -6,27 +8,33 @@ export class Resolver {
 
 	constructor(public event: IEvent) {}
 
-	public created({ pubsub }) {
+	public created({ pubsub }: { pubsub: PubSub }) {
 		return {
 			subscribe: () => pubsub.asyncIterator(this.event.CREATED)
 		}
 	}
 
-	public updated({ pubsub }) {
+	public updated({ pubsub }: { pubsub: PubSub }) {
 		return {
 			subscribe: () => pubsub.asyncIterator(this.event.UPDATED)
 		}
 	}
 
-	public deleted({ pubsub }) {
+	public deleted({ pubsub }: { pubsub: PubSub }) {
 		return {
 			subscribe: () => pubsub.asyncIterator(this.event.DELETED)
 		}
 	}
 
-	public upserted({ pubsub }) {
+	public upserted({ pubsub }: { pubsub: PubSub }) {
 		return {
 			subscribe: () => pubsub.asyncIterator(this.event.UPSERTED)
+		}
+	}
+
+	public publish({ pubsub, events, dataset }: { pubsub: PubSub, events: Array<string>, dataset: Array<object> }) {
+		for (let index = 0; index < events.length; index++) {
+			pubsub.publish(events[index], dataset[index])
 		}
 	}
 }
