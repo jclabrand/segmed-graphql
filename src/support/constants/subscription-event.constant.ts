d20460c32bf1bdadb9a5bae3f4bdd65b4ad89569
Juan Carlos <jcharlylc@gmail.com>
2024-03-22 11:01:14 -0400

    Group subscriptions

--- src/support/constants/subscription-event.constant.ts
+++ src/support/constants/subscription-event.constant.ts
@@ -6,6 +6,14 @@ enum User {
 	UPSERTED = 'USR-UPSERT'
 }
 
+enum Group {
+	CREATED = 'GRP-C',
+	UPDATED = 'GRP-U',
+	DELETED = 'GRP-D',
+	UPSERTED = 'GRP-UPSERT'
+}
+
 export const SubscriptionEvent = {
 	User,
+	Group
 }
